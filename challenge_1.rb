0669ea1a4f9bf9a5746e54a4b7931259cd8ecb14
Abby Farrell <abbykass@gmail.com>
2014-04-25 21:05:44 -0500

    challenge1 draft

--- challenge_1.rb
+++ challenge_1.rb
@@ -13,38 +13,30 @@
 #   we usually have to do some research on how the task is done:
 #   http://en.wikipedia.org/wiki/Mortgage_calculator
 
-r = 3
-term = 10
-amt = 1000
+
+puts 'Hello, let\'s calculate your monthly payment.'
+puts
+
+puts 'Please enter your interest rate without the \'%\' (ex: 4.50):'
+rate = gets.chomp.to_f
+
+puts 'Now enter the length of your loan (in years):'
+nper = gets.chomp.to_i
+
+puts 'Last step, please enter how much money you\'re borrowing:'
+pv = gets.chomp.to_i
 
 def pmt(rate, nper, pv)
-	r = rate_to.f
-	term = nper_to.i
-	amt = pv_to.i
-	payment = ((r*amt) / ((1-(1+r)**-term)))
-
+	# convert rate to percent and monthly
+	r = rate / 12 / 100
+	# numerator
+	n = r * pv
+	# denominator
+	d = 1 - (1+r)**-nper
+	#calc montly payment
+	pmt = n / d
 end
 
-pmt(3,10,1000)
+puts "Your monthly payment will be $#{pmt(rate, nper, pv).round(2)}."
 
-# puts 'Hello, let\'s calculate your monthly payment.'
-# puts
-
-# puts 'Please enter your interest rate without the \'%\' (ex: 4.50):'
-# rate = gets.chomp.to_f
-
-# puts 'Now enter the length of your loan (in years):'
-# nper = gets.chomp.to_i
-
-# puts 'Last step, please enter how much money you\'re borrowing:'
-# pv = gets.chomp.to_i
-
-# end
-# puts (payment)
-
-# puts "Your monthly payment will be $#{pmt(r,term,amt)}."
-
-# Example usage of the method:
-
-# puts "Your monthly payment will be #{pmt(0.01, 60, 30000)}."
 
